896f8fb272f76a7b16c365dc4a86d3b6ae755a6c
D.O <d.o@kakaoenterrpise.com>
2021-04-22 15:52:10 +0900

    reference 앞공간 없앰

--- _plugins/reference.rb
+++ _plugins/reference.rb
@@ -49,7 +49,7 @@ module Jekyll
       end
       context.stack do
         body = super
-        "<a href=\"##{backlink_id(context, @id)}\" class=\"backlink\">[#{@id}]</a>#{body}<br/>"
+        "<a href=\"##{backlink_id(context, @id)}\" class=\"backlink\">[#{@id}]</a> #{body}<br/>"
       end
     end
   end
@@ -64,7 +64,7 @@ module Jekyll
                     else
                       site.getConverterImpl(Jekyll::Converters::Markdown)
                     end
-        "<ol class=\"referencelist\">#{converter.convert(body)}</ol>"
+        "#{converter.convert(body)}"
       end
     end
   end
