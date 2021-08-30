eebab7c068341e1185f27cae82afe7967c8057f7
orange.b <orange.b@kakaoenterprise.com>
2021-08-30 19:08:52 +0900

    불필요한 빌드 결과물 제거

--- docs/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=production -p 4000:4000 jekyll/jekyll:3.8 jekyll build --config _config.yml
--- docs/serve.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=development -p 4000:4000 jekyll/jekyll:3.8 jekyll serve
